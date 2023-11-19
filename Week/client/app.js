a779c0cab68dd38b24e8bf26adecd46a8962602d
Monyjenni <chansovanmonyyoeun03@gmail.com>
2023-11-19 20:40:40 +0700

    Student_Table

--- Week/client/app.js
+++ Week/client/app.js
@@ -1,64 +1,42 @@
 console.log('Hello Mony')
 
+        var lastId = 0;
 
-var lastId = 0;
+        function loadStudents() {
+            let data = [
+                { 'id': 1, 'name': 'mony', 'phone': '1232' },
+                { 'id': 2, 'name': 'mony', 'phone': '1232' },
+                { 'id': 3, 'name': 'mony', 'phone': '1232' },
+                { 'id': 4, 'name': 'mony', 'phone': '1232' }
+            ];
+            return data;
+        }
 
-function loadStudents () {
-  let data = [
-    {
-      'id' : 1,
-      'name': 'mony',
-      'phone': '1232'
-    },
-    {
-      'id' : 1,
-      'name': 'mony',
-      'phone': '1232'
-    },
-    {
-      'id' : 1,
-      'name': 'mony',
-      'phone': '1232'
-    },
-    {
-      'id' : 1,
-      'name': 'mony',
-      'phone': '1232'
-    }
-  ];
-  return data;
-}
+        function loadStudentToTable() {
+            const students = loadStudents();
+            const tableStudent = document.getElementById('tableStudent');
+            
+            for (const student of students) {
+                const rowCount = tableStudent.rows.length;
+                const row = tableStudent.insertRow(rowCount);
+                
+                const cell1 = row.insertCell(0);
+                cell1.innerHTML = student.id;
+                
+                const cell2 = row.insertCell(1);
+                cell2.innerHTML = student.name;
 
-function loadStudentTotable () {
-  const students = loadStudents();
-  const tableStudent = document.getElementById('tableStudent');
-  const rowCount = tableStudent.ariaRowSpan.length;
-  console.log(rowCount)
-  for (students of students) {
-    const rowCount = tableStudent.ariaRowSpan.length;
-    const row = tableStudent.insertRow(rowCount);
-    const cell1 = row.insertCell(0);
-    cell1.innerHTML = student.id
-    lastId = parseInt(student.id)
+                const cell3 = row.insertCell(2);
+                cell3.innerHTML = student.phone;
+                
+                lastId = parseInt(student.id);
+            }
+        }
 
-    const cell2 = row.insertCell();
-    cell2.innerHTML = student.id
-    lastId = parseInt(student.id)
-
-    const cell3 = row.insertCell(2);
-    cell3.innerHTML = student.id
-    lastId = parseInt(student.id)
-
-    const cell4 = row.insertCell(3)
-    //const btnGroup = document.createElement('div');
-
-
-    const btnEdit = document.createElement('button');
-
-  }
-
-}
-
-//init load students 
+        const btnEdit = document.createElement('button');
+        btnEdit.setAttribute('button')
+        // Initialize by loading students to the table
+        loadStudentToTable();
+    
 loadStudentToView();
 
