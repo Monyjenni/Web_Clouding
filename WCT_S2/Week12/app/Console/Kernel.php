437f78c7bd9d9f68497c4eab05d45a803811818d
Monyjenni <chansovanmonyyoeun03@gmail.com>
2024-05-18 13:35:05 +0700

    Feat: CRUD Custom Command

---
+++ WCT_S2/Week12/app/Console/Kernel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        // Add your custom commands here
+        Commands\GenerateCRUD::class,
+    ];
+
+    // Other methods and properties...
+}
